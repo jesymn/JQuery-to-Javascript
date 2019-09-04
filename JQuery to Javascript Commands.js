6ad7ac428149b4ed25047b5bfba861a500bf3533
twistafries <twersterfrehs@gmail.com>
2019-09-04 10:35:45 +0800

    Added new functions
    Added $.each equivalents, as well as grouped contrasting or related functions

--- JQuery to Javascript Commands.js
+++ JQuery to Javascript Commands.js
@@ -20,9 +20,15 @@ const emptyChildren = function(parent) {
     }
 }
 
+/*---------------------------------------------------------/
+/hide and show. Kahit anong valid DOM selector should work /
+/---------------------------------------------------------*/
 // .hide() ---------------------------
 // IDs 
 document.getElementById("id").style.display = "none";
+// .show() ---------------------------
+// IDs
+document.getElementById("id").style.display = "block";
 
 // CLASSES
 const classes = document.getElementsByClassName("class");
@@ -40,12 +46,22 @@ for (let i = 0; i < classes.length; i++) {
     classes[i].classList.remove("newClass");
 }
 
-// .show() ---------------------------
-// IDs
-document.getElementById("id").style.display = "block";
 
 // CLASSES 
 const classes = document.getElementsByClassName("class");
 for (let i = 0; i < classes.length; i++) {
     classes[i].style.display = "block";
 }
+
+/* -- DATA STRUCTURES --- */
+// ITERATING THROUGH DATA STRUCTURES (o ung nagfofor loop ka ganun pero hindi direct na for)
+// jQuery:
+$("Any valid DOM selector").each(function(){})
+
+// vanilla
+// through a variable or constant
+const domSelector = document.getElementsByClassName("exampleClass");
+domSelector.forEach((item) => {
+    // handle each item here
+    // ung 'item' is the current item iteration, kind of like domSelector[i] pag regular for loop
+});
